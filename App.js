f60a9a4575440d9b80ac054cfc2784585f03ff06
Cuonghoclaptrinh <manhcuong2004ml@gmail.com>
2025-01-07 11:08:33 +0700

    Your commit message

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginScreen from './AppReActNative/LoginScreen'; 
+import Bai2 from './Bai2/Bai2_01';
+import NotificationList from './Bai2/Bai2_02';
+
+const App = () => {
+  // return <LoginScreen />;
+  // return <Bai2 />;
+  return <NotificationList />;
+};
+
+export default App;
